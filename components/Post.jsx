3ce8baf54cb7f6324061ca0c89ddd10d5fd9b023
Effiong Bassey <f.young.de@gmail.com>
2021-09-30 09:33:32 +0100

    Display blog titles, images etc on home page

---
+++ components/Post.jsx
@@ -0,0 +1,61 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from '@/styles/Post.module.css'
+
+const Post = ({ post }) => {
+  console.log(post)
+  console.log(post.frontmatter.author_image)
+  return (
+    <div className={styles.post}>
+      <Image
+        src={post.frontmatter.cover_image}
+        height={'420'}
+        width={600}
+        alt="cover image"
+        className="mb-4 rounded"
+      />
+
+      <div className="flex justify-between items-center">
+        <span className="font-light text-gray-600">
+          {post.frontmatter.date}
+        </span>
+        <div>{post.frontmatter.category}</div>
+      </div>
+
+      <div className="mt-2">
+        <Link href={`/blog/${post.slug}`}>
+          <a className="text-2xl text-gray-700 font-bold hover:underline">
+            {post.frontmatter.title}
+          </a>
+        </Link>
+
+        <p className="mt-2 text-gray-600">{post.frontmatter.excerpt}</p>
+      </div>
+
+      <div className="flex justify-between items-center mt-6">
+        <Link href={`/blog/${post.slug}`}>
+          <a className="text-gray-900 hover:text-blue-600">Read more...</a>
+        </Link>
+        <div className="item-center">
+          {/* <Image
+            src={post.frontmatter.author_image}
+            alt="author Image"
+            className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            height={50}
+            width={50}
+          /> */}
+
+          <img
+            src={`${post.frontmatter.author_image}`}
+            alt="author"
+            className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+          />
+          <h3 className="text-gray-700 font-bold">{post.frontmatter.author}</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,51 @@
-import Layout from '@/components/Layout'
+import fs from 'fs'
+import path from 'path'
+import * as matter from 'gray-matter'
+import Link from 'next/link'
 
-export default function HomePage() {
+import Layout from '@/components/Layout'
+import Post from '@/components/Post'
+import styles from '@/styles/HomePage.module.css'
+
+export default function HomePage({ posts }) {
   return (
     <Layout>
-      <div className="bg-red-500 ">Welcome to TailwindCss</div>
+      <div className={styles.homePage}>
+        <div className="text-5xl border-b-4 p-5 font-bold ">Latest Posts</div>
+
+        <div className={styles.postsBox}>
+          {posts.map((post, index) => (
+            <Post key={index} post={post} />
+          ))}
+        </div>
+
+        <Link href="/blog">
+          <a className={styles.link}>All posts</a>
+        </Link>
+      </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    )
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return { slug, frontmatter }
+  })
+
+  console.log(posts)
+
+  return {
+    props: { posts },
+  }
 }
